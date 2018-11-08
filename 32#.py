d3b37f98e0c88830549e4c5541505f25d5fd9fcd
pingulord22 <43420479+pingulord22@users.noreply.github.com>
2018-11-08 14:20:32 -0700

    Update 32#.py

--- 32#.py
+++ 32#.py
@@ -1,1 +1,8 @@
-print("console")
+# Random shuffler test function
+myList = []
+myList.append(input("Input the card that you would like to draw from the pile."))
+userCopy = str(myList)
+def shfl():
+  print(userCopy)
+ 
+  
